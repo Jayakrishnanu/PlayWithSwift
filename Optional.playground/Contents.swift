c6e7c53e501acd9a97d9d5fc939861d9ebc09b9a
jayakrishnanu <jkrishnanu@gmail.com>
2020-06-03 19:42:41 -0700

    Play with swift

---
+++ Optional.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+//
+//Problem
+//
+//Why did the Swift engineers implement optionals?
+//
+//Swift Rules
+//
+//Every variable type must be defined (Implicit/Explicit)
+//The type is inferred based on the value
+
+
+var name: String = "Jayakrishnan" //Explicit
+let newName = "Vinu" //Implicit
+
+let myAge = 34 //Implicit
+let mySon: Int = 1 //Explicit
+let myGPA: Double = 60.0 //Explicit
+
+
+
+
